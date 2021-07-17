5af55e234bbb589a171706ead65f663009c5653d
Bryan Jazo <bryanjazo88@gmail.com>
2021-07-16 20:51:08 -0700

    working on home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import React from 'react'
+import Home from './Components/Home.js'
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-  
+    
+    <Router path='/'>
+        <Home />
+    </Router>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Home(){
+    return (
+      <div><h1>Test</h1></div>
+    )
+
+}
+
+export default Home
