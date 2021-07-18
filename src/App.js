7ace78c37ea16f61804dd7a460b276d6a5ebeddd
Bryan Jazo <bryanjazo88@gmail.com>
2021-07-17 21:09:33 -0700

    adding nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Home from './Components/Home.js'
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import ButtonAppBar from './Components/Navbar.js'
 
 function App() {
   return (
     <div className="App">
-    
+    <ButtonAppBar />
     <Router path='/'>
         <Home />
     </Router>
