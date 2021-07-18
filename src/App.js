bc78aa9e6a09923cff8c7d56227cc07353b3148e
Bryan Jazo <bryanjazo88@gmail.com>
2021-07-17 21:46:24 -0700

    working on Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Home from './Components/Home.js'
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
-import instance from './Components/Navbar.js'
+import {BrowserRouter as Router} from "react-router-dom";
+import navBar from './Components/Navbar.js'
 
 function App() {
   return (
     <div className="App">
-    <instance />
+    <navBar />
     <Router path='/'>
         <Home />
     </Router>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import { Navbar, Dropdown, Icon, Nav } from 'rsuite';
+import { Navbar} from 'rsuite';
+import './Navbar.css'
 
-export const instance = (
-  <Navbar>
-    <Navbar.Header>
-      <a href="#" className="navbar-brand logo">RSUITE</a>
-    </Navbar.Header>
-    <Navbar.Body>
-      <Nav>
-        <Nav.Item icon={<Icon icon="home" />} >Home</Nav.Item>
-        <Nav.Item>News</Nav.Item>
-        <Nav.Item>Products</Nav.Item>
-        <Dropdown title="About">
-          <Dropdown.Item>Company</Dropdown.Item>
-          <Dropdown.Item>Team</Dropdown.Item>
-          <Dropdown.Item>Contact</Dropdown.Item>
-        </Dropdown>
-      </Nav>
-      <Nav pullRight>
-        <Nav.Item icon={<Icon icon="cog" />} >Settings</Nav.Item>
-      </Nav>
-    </Navbar.Body>
-  </Navbar>
-);
+function navBar(){
+
+
+  return(
+    <Navbar className="test">
+      <Navbar.Header>
+        <a href="/" className="navbar-brand logo">AnimeCity</a>
+      </Navbar.Header>
+    </Navbar>
+  )
+}
+
+export default navBar;
