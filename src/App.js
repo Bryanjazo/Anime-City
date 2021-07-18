f3c42a0728ab83d9df032ce9613a70dafa3087e3
Bryan Jazo <bryanjazo88@gmail.com>
2021-07-17 22:22:37 -0700

    navbar add In

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import React from 'react'
 import Home from './Components/Home.js'
 import {BrowserRouter as Router} from "react-router-dom";
 import navBar from './Components/Navbar.js'
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  Sidebar,
+  Navbar,
+  Nav,
+  Icon,
+  Dropdown
+} from "rsuite";
+import "rsuite/dist/styles/rsuite-default.css";
+
 
 function App() {
   return (
     <div className="App">
-    <navBar />
+  <Container className="navbar">
+    <Header>
+      <Navbar >
+        <Navbar.Body>
+          <Nav>
+            <Nav.Item icon={<Icon icon="home" />}>Home</Nav.Item>
+            <Nav.Item>News</Nav.Item>
+            <Nav.Item>Products</Nav.Item>
+            <Dropdown title="About">
+              <Dropdown.Item>Company</Dropdown.Item>
+              <Dropdown.Item>Team</Dropdown.Item>
+            </Dropdown>
+          </Nav>
+          <Nav pullRight>
+            <Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
+          </Nav>
+        </Navbar.Body>
+      </Navbar>
+    </Header>
+
+  </Container>
+
     <Router path='/'>
         <Home />
     </Router>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 function navBar(){
 
   return(
-
+    <div></div>
   )
 }
 
