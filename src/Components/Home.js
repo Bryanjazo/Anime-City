15d85aecf070a339a618a9bccaa3e1fda2a6c969
Bryan Jazo <bryanjazo88@gmail.com>
2021-07-24 16:58:37 -0700

    cleaning warnings

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import './Home.css'
-import {useSelector, useDispatch} from 'react-redux'
-import ModalHome from './Modal.js'
+import {useSelector} from 'react-redux'
+
 import Anime from './Anime.js'
-import {Link, useHistory} from 'react-router-dom'
+
 import {setModalStatus} from '../Redux/reducerRedux.js'
 
 
 function Home(){
   const {modal} = useSelector((state) => state.anime)
-  const history = useHistory()
   const [animes, setAnimes] = useState([])
-  const dispatch = useDispatch()
   useEffect(() =>{
     fetch("http://localhost:3000/animes")
     .then(resp => resp.json())
